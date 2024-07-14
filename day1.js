09b42b10968601f5eced03089e60b27af81bb623
Dipesh <dipeshmittal2610@gmail.com>
2024-07-15 00:26:32 +0530

    day 1 & 2

---
+++ day1.js
@@ -0,0 +1,28 @@
+var int = 22;
+console.log(int);
+
+let str1 = "suppppp";
+console.log(str1);
+
+const boolValue = true;
+console.log(boolValue);
+
+let data1 = "string";
+let data2 = 25;
+let data3 = false;
+let data4 = {
+    name: "string",
+    age: 25,
+};
+let data5 = [1, 2, 3, 4, 5];
+console.log(typeof data1, typeof data2, typeof data3, typeof data4, typeof data5);
+
+let initialValue = "string";
+console.log(initialValue);
+initialValue = 25;
+console.log(initialValue);
+
+const initialValue2 = "string";
+console.log(initialValue2);
+initialValue2 = 25; // Error
+console.log(initialValue2);
---
+++ days2.js
@@ -0,0 +1,32 @@
+// Activity 1
+let num1 = 10, num2 = 5;
+console.log(`The numbers are ${num1} and ${num2}`)
+console.log("Addition:", num1 + num2);       // Task 1
+console.log("Subtraction:", num1 - num2);    // Task 2
+console.log("Multiplication:", num1 * num2); // Task 3
+console.log("Division:", num1 / num2);       // Task 4
+console.log("Remainder:", num1 % num2);      // Task 5
+
+// Activity 2
+let variable = 20;
+variable += 10;
+console.log("Add assignment:", variable);    // Task 6
+variable -= 5;
+console.log("Subtract assignment:", variable); // Task 7
+
+// Activity 3
+let a = 15, b = 10;
+console.log("Greater than:", a > b);         // Task 8
+console.log("Less than:", a < b);            // Task 8
+console.log("Greater or equal:", a >= b);    // Task 9
+console.log("Less or equal:", a <= b);       // Task 9
+console.log("Equal:", a == b);               // Task 10
+
+// Activity 4
+console.log("Logical AND:", a > 10 && b < 20); // Task 11
+console.log("Logical OR:", a < 10 || b > 5);   // Task 12
+console.log("Logical NOT:", !(a < b));         // Task 13
+
+// Activity 5
+let num = -5;
+console.log("Ternary Operator:", num > 0 ? "Positive" : "Negative"); // Task 14
