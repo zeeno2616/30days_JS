0c777db1c5a19064564c1dd2cdc9f06b4863fc2b
Dipesh <dipeshmittal2610@gmail.com>
2024-07-19 22:41:01 +0530

    day 7 (1)

---
+++ day7.js
@@ -0,0 +1,7 @@
+// Task 1: Create an object representing a book with properties like title, author, and year, and log the object to the console.
+const book = {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    year: 1960
+};
+console.log("Book details:", book);
